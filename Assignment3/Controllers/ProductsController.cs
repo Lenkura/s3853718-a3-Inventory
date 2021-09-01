a7fc6dad63f46564e64de123144eadec5af4c580
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-09-01 15:51:49 +1000

    Add product controller and views for listing and creating

---
+++ Assignment3/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Assignment3.Data;
+using Assignment3.Models;
+
+namespace Assignment3.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly A3Context _context;
+
+        public ProductsController(A3Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Product.ToListAsync());
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductID,Name,Price")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Product.Any(e => e.ProductID == id);
+        }
+    }
+}
